ea67cd282bf343e6fed2705ea510070c19c72d13
Yujia <y77cao@YCs-MacBook-Pro.hitronhub.home>
2020-03-03 22:59:16 -0500

    Api usage tutorial

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # React-TODO-App
+1. npm
+2. why use brackets after return in render()?
+3. list out features
+4. https://www.w3schools.com/react/react_lifecycle.asp
+5. Why call APIs in ComponentDidMount()
+6. https://stackoverflow.com/questions/37555031/why-does-json-return-a-promise
+7. https://developers.themoviedb.org/3/genres/get-movie-list
+8. How to filter using navigation bar, React
+9. How to filter using search bar
--- todo-app/src/components/App.js
+++ todo-app/src/components/App.js
@@ -4,15 +4,38 @@ import Navigation from "./Navbar";
 import Movie from "./Movies";
 import Search from "./Search";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      posts: [] // to be deleted
+    }
+  }
 
-  return (
-    <div className = "App">
-      <Navigation search = {Navigation} />
-      <Search search={search} />
-      <p className="App-intro">Top Ranked Movies</p>
+  componentDidMount() {
+    // Fetch data
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((data) => data.json())
+    .then((data) => {
+      this.setState({ posts: data });
+      // console.log(this.state.posts)
+    })
+  }
 
-    </div>
-  );
-
-
+  render() {
+    return (
+      <div className = "App">
+        <Navigation search = {Navigation} />
+        <Search search={() => {} /* need to implement search function */} />
+        <p className="App-intro">Top Ranked Movies</p>
+        <ul>
+        {this.state.posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 export default App;
